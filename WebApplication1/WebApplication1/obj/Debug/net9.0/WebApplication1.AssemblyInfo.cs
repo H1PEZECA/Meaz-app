1d8daf1e6ab4a617f9143fd5c1fd52198c4de768
HYPE_ZECA <braabozeca@gmail.com>
2024-12-07 20:44:37 -0300

    21:00 /07/12/2024

--- WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40346671fbc927281accb7470ec8d3456f829162")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33bf018082a8844e6157aa798b638cfdaff9349")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
