eb799395f6072a7bb8a1f453462c3d5daad9142a
HYPE_ZECA <braabozeca@gmail.com>
2024-12-07 20:20:18 -0300

    Create ProdutoViewModel.cs

---
+++ WebApplication1/WebApplication1/Models/ProdutoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class ProdutoViewModel
+    {
+        public string Nome {  get; set; }      // nome do produto
+        public double Preco { get; set; }      // preço do produto 
+        public string ImagemUrl { get; set; }  // imagem do produto
+    }
+}
