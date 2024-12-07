0082dcdf9bf05517680e606a024afbab421aa5ae
HYPE_ZECA <braabozeca@gmail.com>
2024-12-07 20:36:07 -0300

    f

--- WebApplication1/WebApplication1/Models/ProdutoViewModel.cs
+++ WebApplication1/WebApplication1/Models/ProdutoViewModel.cs
@@ -2,8 +2,8 @@
 {
     public class ProdutoViewModel
     {
-        public string Nome {  get; set; }      // nome do produto
-        public double Preco { get; set; }      // preço do produto 
+        public string? Nome {  get; set; }      // nome do produto
+        public double Preco { get; set; }      // preço do produto
         public string ImagemUrl { get; set; }  // imagem do produto
     }
 }
